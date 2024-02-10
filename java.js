cca93cf76ed6770f940fbcb1099e9ea9dfa46c6a
Laxmi Singh <155379742+ilaxmisingh@users.noreply.github.com>
2024-02-10 07:57:38 -0800

    Update java.js

--- java.js
+++ java.js
@@ -1,17 +1,1 @@
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    // Fetch data from form
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    // You can perform further validation or send this data to a server for processing
-    console.log('Username:', username);
-    console.log('Email:', email);
-    console.log('Password:', password);
-  
-    // Reset the form
-    document.getElementById('registrationForm').reset();
-  });
-  
+.
